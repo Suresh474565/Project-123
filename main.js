79dc837aa248b6a30c4ded46562cf6fe9557c724
Suresh474565 <85395556+Suresh474565@users.noreply.github.com>
2022-10-21 15:37:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,11 +29,40 @@ console.log("model has been loaded");
 
 function gotPoses(results)
 {
+{
+
+console.log(results);
+
+}
+
+}
+
+function draw()
+{
+
+background('#21cca4');
+textSize(difference);
+fill('#2456a6');
+text('MANISH',x_wristL,x_wristR);
+
+}
+
+function gotPoses(results)
+{
 
 if(results.length>0)
 {
 
 console.log(results);
+nose_x=results[0].pose.nose.x;
+nose_y=results[0].pose.nose.y;
+console.log("Nose X value ="+nose_x + "Nose y value =" + nose_y);
+
+x_wristL=results[0].pose.leftWrist.x;
+x_wristR=results[0].pose.rightWrist.x;
+difference= floor(x_wristL - x_wristR);
+
+console.log("Right wrist X value =" + x_wristR + "Right wrist X value =" + x_wristL + "difference ="+difference);
 
 }
 
